5fa26571b0f6644227ff4c5a47164b3590da7885
feng626 <1304903146@qq.com>
2021-10-22 11:19:17 +0800

    fix: rdp固定名称问题

--- Windows/JumpServer.py
+++ Windows/JumpServer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 import sys
 import json
 import base64
@@ -8,17 +9,26 @@ import subprocess
 
 '''
 {
- "protocol": "ssh",
- "username": "laoguang",
- "token": "xxx",   // 认证 授权
- "config": "full address:s:rdjumpserver.fit2cloud.com:33390"
+    "filename": "{}-{}-jumpserver".format(username, name),
+    "protocol": "ssh",
+    "username": "laoguang",
+    "token": "xxx",   // 认证 授权
+    "config": "full address:s:rdjumpserver.fit2cloud.com:33390"
 }
 '''
 BASE_DIR = os.path.dirname(__file__)
 
 
+def remove_current_rdp_file():
+    re_rdp_file = re.compile(r'.*\.rdp$')
+    for filename in os.listdir(BASE_DIR):
+        if re_rdp_file.search(filename):
+            os.remove(os.path.join(filename))
+
+
 class Rouse(object):
     def __init__(self, data):
+        self.filename = data.get('filename', 'jms')
         self.protocol = data['protocol']
         self.config = data['config']
 
@@ -26,7 +36,7 @@ class Rouse(object):
         pass
 
     def handle_rdp(self):
-        file_path = os.path.join(BASE_DIR, 'client.rdp')
+        file_path = os.path.join(BASE_DIR, self.filename + '.rdp')
         with open(file_path, 'w') as f:
             f.write(self.config)
         if platform.system().lower() == 'windows':
@@ -39,10 +49,12 @@ class Rouse(object):
 
 
 if __name__ == '__main__':
-
     args = sys.argv
     if len(args) != 1:
         d = args[1].replace('jms://', '')
+        if d[-1] == '/':
+            d = d[:-1]
         d = json.loads(base64.b64decode(d).decode())
+        remove_current_rdp_file()
         instance = Rouse(d)
         instance.run()
