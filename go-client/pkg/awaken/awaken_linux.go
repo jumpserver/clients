d5a007bcfb15d59a905c933655efb6b4d81ba3b4
halo <wuyihuangw@gmail.com>
2023-11-10 23:36:10 +0800

    perf: 支持xfreerdp忽略证书

--- go-client/pkg/awaken/awaken_linux.go
+++ go-client/pkg/awaken/awaken_linux.go
@@ -31,7 +31,8 @@ func awakenRDPCommand(filePath string, cfg *config.AppConfig) *exec.Cmd {
 	if appItem == nil {
 		return nil
 	}
-	cmd := exec.Command(appItem.Name, filePath)
+	args := strings.Replace(argFormat, "{file}", filePath, 1)
+	cmd := exec.Command(appItem.Name, strings.Split(args, " ")...)
 	return cmd
 }
 
