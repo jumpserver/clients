552435ba1ea50a61de495f27396977530ecad04c
halo <wuyihuangw@gmail.com>
2024-12-09 15:29:44 +0800

    perf: update drawer err

--- new-Client/src/renderer/src/layouts/index.vue
+++ new-Client/src/renderer/src/layouts/index.vue
@@ -7,6 +7,7 @@
       <n-layout-content>
         <header-section :active="active" />
         <router-view :active="active" />
+        <div id="drawer-target"></div>
       </n-layout-content>
     </n-layout>
   </n-layout>
