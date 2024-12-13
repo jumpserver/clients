4cd5088645d93b1c26d3460361ba3ccde1c6a5a8
ZhaoJiSen <97007455+ZhaoJiSen@users.noreply.github.com>
2024-12-13 12:24:43 +0800

    Pr@dev@fixed account connect (#68)
    * fixed: Restrictions on account connections
    
    * perf: Add account input

--- ui/src/renderer/src/components/MainSection/helper/index.ts
+++ ui/src/renderer/src/components/MainSection/helper/index.ts
@@ -1,11 +1,24 @@
-import { h } from 'vue';
-import { NText, NIcon, NButton } from 'naive-ui';
+import { computed, h, ref } from 'vue';
+import {
+  NText,
+  NIcon,
+  NButton,
+  NForm,
+  NFormItem,
+  NInput,
+  ConfigProviderProps,
+  lightTheme,
+  darkTheme,
+  createDiscreteApi
+} from 'naive-ui';
 import mittBus from '@renderer/eventBus';
 
 import type { Ref } from 'vue';
 import type { Component } from 'vue';
 import type { DropdownOption } from 'naive-ui';
 import type { IItemDetail } from '@renderer/components/MainSection/interface';
+
+import { Conf } from 'electron-conf/renderer';
 
 export const renderCustomHeader = (
   component: Component,
@@ -92,4 +105,118 @@ export const moveElementToEnd = (arr: DropdownOption[], searchKey: string, chang
   }
 
   return arr;
+};
+
+export const useAccountModal = (type: string) => {
+  const conf = new Conf();
+
+  const defaultTheme = ref('');
+  const inputPassword = ref('');
+  const inputUsername = ref('');
+
+  conf.get('defaultSetting').then(res => {
+    if (res) {
+      // @ts-ignore
+      defaultTheme.value = res?.theme;
+    }
+  });
+
+  const configProviderPropsRef = computed<ConfigProviderProps>(() => ({
+    theme: defaultTheme.value === 'light' ? lightTheme : darkTheme
+  }));
+
+  const { modal } = createDiscreteApi(['modal'], {
+    configProviderProps: configProviderPropsRef
+  });
+
+  const modalTitle = type === '手动输入' ? '手动输入账号密码' : '输入账号密码';
+
+  const handleConfirm = () => {
+    console.log(1);
+  };
+
+  modal.create({
+    title: modalTitle,
+    preset: 'card',
+    bordered: false,
+    segmented: true,
+    style: {
+      width: '30rem',
+      borderRadius: '10px'
+    },
+    content: () =>
+      h(
+        NForm,
+        {
+          labelPlacement: 'top',
+          labelWidth: 80,
+          size: 'small'
+        },
+        {
+          default: () => [
+            h(
+              NFormItem,
+              {
+                label: '用户名',
+                path: 'username'
+              },
+              {
+                default: () =>
+                  h(NInput, {
+                    value: inputUsername.value,
+                    placeholder: '用户名'
+                  })
+              }
+            ),
+            h(
+              NFormItem,
+              {
+                label: '密码',
+                path: 'password'
+              },
+              {
+                default: () =>
+                  h(NInput, {
+                    value: inputPassword.value,
+                    type: 'password',
+                    showPasswordOn: 'click',
+                    placeholder: '请输入密码',
+                    onUpdateValue: value => {
+                      inputPassword.value = value;
+                    }
+                  })
+              }
+            ),
+            h(
+              NFormItem,
+              {
+                style: {
+                  display: 'flex',
+                  justifyContent: 'flex-end'
+                }
+              },
+              {
+                default: () =>
+                  h(
+                    NButton,
+                    {
+                      type: 'primary',
+                      size: 'medium',
+                      onClick: () => handleConfirm()
+                    },
+                    {
+                      default: () => '确认'
+                    }
+                  )
+              }
+            )
+          ]
+        }
+      )
+  });
+
+  return {
+    inputPassword: inputPassword.value,
+    inputUsername: inputUsername.value
+  };
 };
