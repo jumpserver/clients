1832ab5115b8ab24a4585969ce518a4f4a21b0b1
zhaojisen <1301338853@qq.com>
2025-07-11 17:08:34 +0800

    Change login

--- ui/src/renderer/src/App.vue
+++ ui/src/renderer/src/App.vue
@@ -45,8 +45,6 @@ const {
   switchAccount,
   handleModalOpacity,
   handleCredentialsReceived,
-  setupCookiesForSite,
-  handleCookieReceived,
   restoreSavedCookies
 } = useUserAccount();
 
@@ -141,7 +139,7 @@ onMounted(async () => {
   }
 
   // 恢复保存的 cookie（在检查登录状态之前）
-  restoreSavedCookies();
+  await restoreSavedCookies();
 
   // 检查是否需要显示登录框
   if (!userStore.session || (userStore.userInfo && userStore.userInfo.length <= 0)) {
@@ -157,15 +155,6 @@ onMounted(async () => {
     (_e, credentials: { session: string; csrfToken: string; site: string }) =>
       handleCredentialsReceived(credentials)
   );
-
-  window.electron.ipcRenderer.on(
-    'set-login-cookies',
-    (_e, credentials: { csrfToken: string; site: string; cookies: Electron.Cookie[] }) => {
-      handleCookieReceived(credentials);
-    }
-  );
-
-  window.electron.ipcRenderer.on('setup-cookies-for-site', () => setupCookiesForSite());
 
   mittBus.on('changeLang', handleLangChange);
   mittBus.on('changeTheme', handleThemeChange);
